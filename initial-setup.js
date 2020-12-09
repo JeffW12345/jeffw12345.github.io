69ce28795e862f87def2bc9a1c6ee6f98bc43bde
Jeff Waters <watersjg@gmail.com>
2020-12-09 18:42:57 +0000

    Used JavaScript to produce initial board

---
+++ initial-setup.js
@@ -0,0 +1,14 @@
+var board = ["green","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty",
+"empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty", 
+"empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty", "empty",
+"empty","empty","empty","empty","empty","empty","empty","red","empty","empty","empty","empty","empty","empty","red"]
+
+let a = 1
+board.forEach(myFunction)
+
+function myFunction(item) {
+  if (item == "empty"){document.write('<div class = "square"></div>');}
+  if (item == "red"){document.write('<div class = "square"><div class = "red-circle"></div></div>');}
+  if (item == "gold"){document.write('<div class = "square"><div class = "gold-circle"></div></div>');}
+  if (item == "green"){document.write('<div class = "square"><div class = "green-circle"></div></div>');}
+}
