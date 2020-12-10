5333c961334e4c8747eec8604bbb0b57f270c05c
Jeff Waters <watersjg@gmail.com>
2020-12-10 07:37:54 +0000

    Further JavaScript coding

--- game.js
+++ game.js
@@ -1,16 +1,20 @@
 //This file awaits for an instruction to begin the game, and then runs the game.
 
+awaitNewGameInstruction()
 
 awaitNewGameInstruction(){
-	document.getElementById("NEW-GAME").addEventListener("mousedown",startGame());
+	removePreviousGameMessages();
+	document.getElementById("NEW-GAME").addEventListener("mousedown",startGame());}
 }
 
 startGame(){
-	createBoard();
+	document.getElementById("NEW-GAME").addEventListener("mousedown",newGameClicked());}
+	createInitialBoard();
 	var gameInProgress = true;
 	var lastTimeRecorded = new Date.now();
 	var circleObjects[]; // To store human player, computer player and gold circle objects. 
 	circleObjects[0] = newHumanPlayerObject();
+	circleObjects[1] = newGoldCircleObject(findFreeSquare());
 	var score = 0;
 	var numberOfComputerOppoments = 2;
 	while gameInProgress == true{
@@ -29,12 +33,17 @@ function newComputerPlayer(initialSquare){
 function newHumanPlayer(){
 	this.square = 1;}
 
-function newGoldCircleObject(initialSquare){
-	this.square = initialSquare;} //TBC
+function newGoldCircleObject(squareNum){
+	this.square = squareNum;} //TBC
 
 function isSquareFree(circleObjects[]){
 	//Checks if a square is free for a new object to occupy. TBC
 }
 
-function setSquare() (;) //TBC
+function setSquare() {;} //Sets a new square for the 'this' object. 
+
+function removePreviousGameMessages() {;} //TBC
+
+//Returns the number of a random square not currently occupied. 
+function findFreeSquare(){}
 
