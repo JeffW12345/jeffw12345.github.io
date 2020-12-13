6bf4b06604b737c3588f708c839039fe6344cd80
Jeff Waters <watersjg@gmail.com>
2020-12-13 18:15:40 +0000

    JavaScript debugging

--- game.js
+++ game.js
@@ -2,26 +2,30 @@
 
 awaitNewGameInstruction()
 
-awaitNewGameInstruction(){
+function awaitNewGameInstruction(){
 	removePreviousGameMessages();
+	//TO DO - Code for writing score. 
 	document.getElementById("NEW-GAME").addEventListener("mousedown",startGame());}
-}
 
-startGame(){
-	document.getElementById("NEW-GAME").addEventListener("mousedown",newGameClicked());}
+function startGame(){
+	document.getElementById("NEW-GAME").addEventListener("mousedown",newGameClicked());
 	createInitialBoard();
 	var gameInProgress = true;
 	var lastTimeRecorded = new Date.now();
-	var circleObjects[]; // To store human player, computer player and gold circle objects. 
-	circleObjects[0] = newHumanPlayerObject();
-	circleObjects[1] = newGoldCircleObject(findFreeSquare());
-	var score = 0;
+	var circleObjects = []; // To store human player, computer player and gold circle objects. 
+	circleObjects[0] = {type: 'green', square: 0};  //Human player
+	circleObjects[1] = {type: 'gold', square: findFreeSquare()}; // Gold square 
+	circleObjects[2] = {type: 'red', square: 63}; // Red square - target for human
+	circleObjects[3] = {type: 'red', square: 56}; // Red square - computer opponent
 	var numberOfComputerOppoments = 2;
-	while gameInProgress == true{
+	var score = 0;
+	while (gameInProgress){
 		currentTime = new Date.now()
 		if(currentTime <  lastTimeRecorded + 500) {continue;}  // Has half a second passed since the last loop?
-	};//TBC
+	}
+	//TBC
 }
+
 
 function playerEliminated(){;} //TBC
 
--- initial-setup.js
+++ initial-setup.js
@@ -1,19 +1,18 @@
 // Initial board setup.
 
-createBoard();
+createInitialBoard();
 
-function createBoard(){
+function createInitialBoard(){
 	// In the array below, the first entry represents the square at the top left row, then next one the next square, etc.
-	// Green = contains a green circle. 
-	var board = ["green","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty",
+	// Green = contains a green circle, etc. 
+	var square, board = ["green","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty",
 	"empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty", 
 	"empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty", "empty",
 	"empty","empty","empty","empty","empty","empty","empty","red","empty","empty","empty","empty","empty","empty","red"];
-	board.forEach(loopThroughBoard);}
-
-function loopThroughBoard(item) {
-  if (item == "empty"){document.write('<div class = "square"></div>');}
-  if (item == "red"){document.write('<div class = "square"><div class = "red-circle"></div></div>');}
-  if (item == "gold"){document.write('<div class = "square"><div class = "gold-circle"></div></div>');}
-  if (item == "green"){document.write('<div class = "square"><div class = "green-circle"></div></div>');}
+	for(square in board){
+		if (board[square] == "empty"){document.write('<div class = "square"></div>');}
+		if (board[square] == "red"){document.write('<div class = "square"><div class = "red-circle"></div></div>');}
+		if (board[square] == "gold"){document.write('<div class = "square"><div class = "gold-circle"></div></div>');}
+		if (board[square] == "green"){document.write('<div class = "square"><div class = "green-circle"></div></div>');}
+	}
 }
