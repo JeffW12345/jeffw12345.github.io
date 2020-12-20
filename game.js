7fa6eb3c70a5fdae002f67dcf3dc48cb2978f14d
Jeff Waters <watersjg@gmail.com>
2020-12-20 12:41:32 +0000

    New functions coded

--- game.js
+++ game.js
@@ -1,14 +1,13 @@
-// In the array below, the first entry represents the square at the top left row, then next one the next square to the left, etc.
+// In the 'board' array below, the first entry represents the square at the top left row, then next one the next square to the left, etc.
 // Green = contains a green circle, etc. 
-
 var square, board = ["green","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty",
 "empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty",
 "empty", "empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty","empty", "empty",
 "empty","empty","empty","empty","empty","empty","empty","red","empty","empty","empty","empty","empty","empty","gold"] 
-
 var gameInProgress = false 
-var humanDirection = 4  // 0 = up, 1 = down, 2 = left, 3 = right, 4 = no move
+var humanDirection = "NO MOVEMENT"
 var circleObjects = [] // To store human player, computer player and gold circle objects.
+circleObjects[0] = {type: 'green', squareNum: 0} // Human player
 var millisecondsElapsed = 0
 var score = 0
 var numberOfComputerOpponents = 1
@@ -22,31 +21,25 @@ function createInitialBoard(){
 	}
 }
 
+// This function is only called when the human player makes a valid initial move. 
+
 function startGame(){
-	addActionListeners
-	gameInProgress = true
-	humanDirection = 4   
-	circleObjects[0] = {type: 'green', square: 0} // Human player
-	circleObjects[1] = {type: 'gold', square: 63} // Gold square (human wins points on reaching this square)
-	circleObjects[2] = {type: 'red', square: 56} // Red square - computer player (human's opponent)
-	// inGameActions() function runs immediately, and then every 500 milliseconds thereafter. 
+	gameInProgress = true  
+	circleObjects[1] = {type: 'gold', squareNum: 63} // Gold square (human wins points on reaching this square)
+	circleObjects[2] = {type: 'red', squareNum: 56} // Red square - computer player (human's opponent)
+	// The inGameActions() function runs immediately, and then every 500 milliseconds thereafter. 
 	inGameActions()
-	if (gameInProgress){setInterval(function(){inGameActions()},500)}
-}
+	setInterval(function(){inGameActions()},500)}
 
 function inGameActions(){
 		millisecondsElapsed += 500
-		// Message if human selects invalid move
-		if (!isHumanMoveValid){
-			document.getElementById("message1").textContent = "Invalid move"
-			humanDirection = 4}
-		else {document.getElementById("message1").textContent = " "}
-		setNewHumanSquare()
+		gameInProgress = true
+		setNewHumanSquare(humanDirection)
 		computerPlayerMoves()
 		winsPointsActions() // If human player has landed on a square containing a gold circle.
 		humanLoses() // If the human player has collided with a computer player.
 		// New computer oppoent added every 20 seconds till there are 4 computer opponents.
-		if (millisecondsElapsed % 20000 == 0 && numberOfComputerOppoments < 5){
+		if (millisecondsElapsed % 20000 == 0 && numberOfComputerOpponents < 5){
 			newComputerPlayer(findFreeSquare())
 			numberOfComputerOpponents++}
 		updateBoard() // Re-populates the board
@@ -54,22 +47,44 @@ function inGameActions(){
 		var humanDirection = 4 // Resetting human movement to stationary post-move
 }
 
-function setNewHumanSquare() {}
+function setNewHumanSquare(directionOfTravel) {
+	
+} //TBC
 
 function humanLoses() {} // TBC
 function winsPointsActions() {} // TBC
 function updateBoard(){} //TBC
 
-function isHumanMoveValid(directionOfMovement){} //TBC
+function isHumanMoveValid(directionOfMovement){
+	var currentSquare = circleObjects[0].squareNum
+	// Checks situations where the move would be out of bounds
+	var onTopRow = currentSquare <= 7
+	if (onTopRow && directionOfMovement = "UP")
+		{return false}
+	var onFarLeftCol = (currentSquare % 8 == 0)
+	if (onFarLeftCol && directionOfMovement = "LEFT")
+		{return false}
+	var farRightColSquareNums = [7, 15, 23, 31, 39, 47, 55, 63]
+	var onFarRightCol = farRightColSquareNums.includes(currentSquare)
+	if (onFarRightCol && directionOfMovement = "RIGHT")
+		{return false}
+	var onBottomRow = (currentSquare >= 56 && currentSquare <= 63)
+	if (onBottomRow && directionOfMovement = "DOWN")
+		{return false}} 
 
-function addDirectionEventListeners(userChoice){
-	if (gameInProgress){
-		if(userChoice == 'UP') {humanDirection = 0}
-		if(userChoice == 'DOWN') {humanDirection = 1}
-		if(userChoice == 'LEFT') {humanDirection = 2}
-		if(userChoice == 'RIGHT') {humanDirection = 3}
+function setHumanDirection(userChoice){
+		if(userChoice == 'UP' && isHumanMoveValid(userChoice)) {humanDirection = userChoice}
+		if(userChoice == 'DOWN'&& isHumanMoveValid(userChoice)) {humanDirection = userChoice}
+		if(userChoice == 'LEFT' isHumanMoveValid(userChoice)) {humanDirection = userChoice}
+		if(userChoice == 'RIGHT' isHumanMoveValid(userChoice)) {humanDirection = userChoice}
+		// Message if human selects invalid move
+		if (!isHumanMoveValid(userChoice){
+			document.getElementById("message1").textContent = "Invalid move"}
+		else {
+			document.getElementById("message1").textContent = "You clicked " + userChoice}
+		// New game is move is valid and game not already in progress. 
+		if (humanDirection != "NO MOVEMENT" && !gameInProgress){startGame()} 
 		}
-	}
 
 function humanPlayerMove(){} //TBC
 
@@ -112,9 +127,9 @@ function removePreviousGameMessages() { } //TBC
 function findFreeSquare(){} //TBC
 
 function addActionListeners() {
-	if (isHumanMoveValid("UP")) {document.getElementById("UP").addEventListener("click", addDirectionEventListeners("UP"))}
-	if (isHumanMoveValid("DOWN")) {document.getElementById("DOWN").addEventListener("click", addDirectionEventListeners("DOWN"))}
-	if (isHumanMoveValid("LEFT")) {document.getElementById("LEFT").addEventListener("click", addDirectionEventListeners("LEFT"))}
-	if (isHumanMoveValid("RIGHT")) {document.getElementById("RIGHT").addEventListener("click", addDirectionEventListeners("RIGHT"))}
-	document.getElementById("NEW-GAME").addEventListener("click", function() {location.reload})
+	document.getElementById("UP").addEventListener("click", setHumanDirection("UP"))
+	document.getElementById("DOWN").addEventListener("click", setHumanDirection("DOWN"))
+	document.getElementById("LEFT").addEventListener("click", setHumanDirection("LEFT"))
+	document.getElementById("RIGHT").addEventListener("click", setHumanDirection("RIGHT"))
+	document.getElementById("RESTART").addEventListener("click", function() {location.reload})
 }
